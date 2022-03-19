6a03104c58376e27a012d4e62eaf458c49763ce5
King <95382450+KingMunching@users.noreply.github.com>
2022-03-18 21:41:34 -0400

    Add files via upload

---
+++ GuessingGame/AttemptsScreen.java
@@ -0,0 +1,14 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttemptsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attempts_screen);
+    }
+}
---
+++ GuessingGame/DifficultyScreen.java
@@ -0,0 +1,14 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DifficultyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_screen);
+    }
+}
