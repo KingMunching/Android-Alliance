675bcc15c6d2e938110c3f0af6e0e4ad2ffb01fd
Jacky <Jackylejjj@gmail.com>
2022-03-27 20:00:23 -0400

    1-10 commit

--- app/src/main/java/com/example/testing/GameScreen.java
+++ app/src/main/java/com/example/testing/GameScreen.java
@@ -3,8 +3,35 @@ package com.example.testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class GameScreen extends AppCompatActivity {
+    int n;
+    String message = "";
+
+    public void randomNumberGenerator(){
+        Random rand = new  Random();
+        n = rand.nextInt(10)+1;
+    }
+
+    public void guess(View view){
+        EditText guess = (EditText) findViewById(R.id.guess);
+        int guessInt = Integer.parseInt(guess.getText().toString());
+
+        if (n != guessInt){
+            message = "guess again";
+        }
+        else {
+            message = "Correct!";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
