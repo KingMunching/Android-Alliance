86abec82f678657780a7b9bd1aa1df2aa4e9c895
ray <raymondlinschool@gmail.com>
2022-04-01 17:20:55 -0400

    ray

--- app/src/main/java/com/example/testing/Username.java
+++ app/src/main/java/com/example/testing/Username.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class Username extends AppCompatActivity {
 
@@ -15,18 +17,22 @@ public class Username extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username);
 
-        button = findViewById(R.id.username_button);
+        button = findViewById(R.id.UsernameBox);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toAttemptsScreen();
+                String uS = button.getText().toString();
             }
         });
 
+
     }
+
     public void toAttemptsScreen(){
         Intent i = new Intent(this, AttemptsScreen.class);
         startActivity(i);
     }
+
 }
